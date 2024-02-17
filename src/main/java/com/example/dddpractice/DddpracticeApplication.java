0ff17846de4a556396c7827f68a9a2d6bf78da08
Ailes16 <ailes.the16thclass@i.softbank.jp>
2024-02-17 21:52:29 +0900

    Initial commit.

---
+++ src/main/java/com/example/dddpractice/DddpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddpracticeApplication.class, args);
+	}
+
+}
